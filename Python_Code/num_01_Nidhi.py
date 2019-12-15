3255c43fd0f866f39f28156434fcf41c2549ac70
Nidhi Mishra <35249244+nidhim12@users.noreply.github.com>
2019-12-15 23:00:30 +0530

    Updated the code

--- Python_Code/num_01_Nidhi.py
+++ Python_Code/num_01_Nidhi.py
@@ -1,4 +1,10 @@
-sumofnum = 0
-for i in range(1,1000):
-    if (i % 3 == 0) or (i % 5 ==0):
-        sumofnum = sumofnum + i;
+t = int(input().strip()) 
+for a0 in range(t):
+    n = int(input().strip()) 
+    sum =0 
+    a = ((n-1)//3) 
+    b = ((n-1)//5) 
+    c = ((n-1)//15) 
+    sum = (((3 * a * (a+1))//2) + ((5 * b * (b+1))//2) - ((15 * c * (c+1))//2)) 
+    print(int(sum))
+
